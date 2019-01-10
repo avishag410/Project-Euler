7fe0a85b135cabcb1ce5fb9d77c3417c4166547b
Avishag <avishag212@gmail.com>
2019-01-10 15:38:32 +0200

    code refactoring

--- problem25.js
+++ problem25.js
@@ -1,17 +1,25 @@
-let f1 = 1;
-let f2 = 1;
-let sum = 0;
-let indexCnt = 2;
-
-while (!isDigitNum(sum, 1000)) {
-    sum = f1 + f2;
-    f1 = f2;
-    f2 = sum;
-    indexCnt++;
-}
-
-console.log(`Index: ${indexCnt}, number = ${sum}`);
-
 function isDigitNum(num, digitNum) {
     return (num.toString().length === digitNum)
 }
+
+function NDigitFibonacciNumber(digitNum)
+{
+    let f1 = 1;
+    let f2 = 1;
+    let sum = 0;
+    let indexCnt = 2;
+    
+    while (!isDigitNum(sum, digitNum)) {
+        sum = f1 + f2;
+        f1 = f2;
+        f2 = sum;
+        indexCnt++;
+    }
+    
+    return [indexCnt, sum];
+}
+
+const ans =  NDigitFibonacciNumber(4);
+const index = ans[0];
+const fibNum = ans[1];
+console.log(`Index: ${index}, number = ${fibNum}`);
